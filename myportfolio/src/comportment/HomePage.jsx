da8a0bb5b9a74dc26949a9a997c3bdb59c019129
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 23:29:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/HomePage.jsx
+++ myportfolio/src/comportment/HomePage.jsx
@@ -3,17 +3,26 @@ import MyImg from '../assets/MyImg.jpg'
 
 const HomePage = () => {
     const styles = {
-        background: `linear-gradient(to right, #011120, rgba(0,0,0,0.4)), url(${MyImg})`,
+        background: `linear-gradient(to right, #081B28, rgba(0,0,0,0.4)), url(${MyImg})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
     }
   return (
     <div>
         <div className="md:grid grid-cols-2">
-            <div className="bg-[#011120]">
-                
+            <div className="hidden md:block bg-[#081B28] text-white pb-40 over-flow-hidden px-16">
+                <div className="mx-16">
+                    <h1 className="md:mt-40 md:text-[70px] font-semibold">Jehan Weerasuriya</h1>
+                    <p className="text-4xl text-blue-500 font-semibold">Frontend && Backend Web Developer</p>
+                    <p className="">Founder and CEO at First Sri Lankan CSS Framework</p>
+                </div>
             </div>
-            <div className="py-10 px-12 h-screen" style={styles}>
+            <div className="py-10 px-6 h-screen" style={styles}>
+                <div className="md:hidden block my-40">
+                    <h1 className="text-3xl font-semibold text-white py-1">Jehan Weerasuriya</h1> 
+                    <p className="text-md text-blue-500 font-semibold">Frontend && Backend Web Developer</p>                   
+                    <p className="text-sm text-white">Founder and CEO at First Sri Lankan CSS Framework</p>
+                </div>
             </div>
         </div>
     </div>
