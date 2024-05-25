4b8e881de889f72e7443e3dd280658d87ca5557f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 22:43:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/HomePage.jsx
+++ myportfolio/src/comportment/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import MyImg from '../assets/MyImg.jpg'
 
 const HomePage = () => {
   return (
     <div>
         <div className="md:grid grid-cols-2 gap-4">
             <div className="">
-                
+                <img src={MyImg} alt="" className='w-full h-full' />
             </div>
         </div>
     </div>
