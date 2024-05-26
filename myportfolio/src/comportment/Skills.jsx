724be177c6dfaeeb2f263d5e3534800e1593f35b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 08:07:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/Skills.jsx
+++ myportfolio/src/comportment/Skills.jsx
@@ -4,7 +4,7 @@ import { BsActivity, BsGithub, BsGlobe } from "react-icons/bs";
 const Skills = () => {
   return (
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
-        <h1 className="md:text-6xl text-2xl text-[#00A0DF] font-semibold flex"> <BsActivity /> <span className=' italic pl-2 font-semibold'>Skills</span></h1>
+        <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex"> <BsActivity /> <span className=' italic pl-2 font-semibold'>Skills</span></h1>
     </div>
   )
 }
