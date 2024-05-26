09f018f4f448600e1bf2ffb258c1ba050a2d8c8c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 09:39:50 +0530

    Update Education.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/Education.jsx
+++ myportfolio/src/comportment/Education.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BsGithub, BsGlobe, BsPerson } from "react-icons/bs";
+import { BsGithub, BsGlobe, BsMortarboard, BsPerson } from "react-icons/bs";
 
 
 const Education = () => {
   return (
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
-        <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsMa /><span className=' italic pl-2 font-semibold'>Business Ownerships</span></h1>
+        <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsMortarboard /><span className=' italic pl-2 font-semibold'>Business Ownerships</span></h1>
         <div className="my-12 md:grid grid-cols-3 gap-4 md:mx-16">
 
         </div>
