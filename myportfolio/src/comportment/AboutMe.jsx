51612680807549ab920fc87c8366705cb2c64a37
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 07:26:22 +0530

    Update AboutMe.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/AboutMe.jsx
+++ myportfolio/src/comportment/AboutMe.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const AboutMe = () => {
   return (
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
-        <h1 className="md:text-3xl text-center text-[#00A0DF]">About Me</h1>
+        <h1 className="md:text-6xl text-2xl text-center text-[#00A0DF] font-semibold">About Me</h1>
+        <div className="md:grid grid-cols-2 gap-4 md:my-8 my-4">
+            <div className="">
+                <h1 className="text-4xl">Hi, <span className='text-[#00A0DF] italic'>I am Jehan Weerasuriya</span></h1>
+            </div>
+        </div>
     </div>
   )
 }
