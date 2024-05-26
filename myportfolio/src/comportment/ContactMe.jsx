b39b09e97fbfed539cac2aea2cc53812e12803ff
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 13:56:25 +0530

    Create ContactMe.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ myportfolio/src/comportment/ContactMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsPersonLinesFill } from 'react-icons/bs'
+
+const ContactMe = () => {
+  return (
+    <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
+        <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsPersonLinesFill /><span className=' italic pl-2 font-semibold'>Contact Me</span></h1>
+        <div className="my-12 md:grid grid-cols-3 gap-4 md:mx-16">
+
+        </div>
+    </div>
+  )
+}
+
+export default ContactMe
