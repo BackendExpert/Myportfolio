48a9a8cd5e136892a97b2184ce4a1c6f048462ad
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 09:28:08 +0530

    Update Business.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/Business.jsx
+++ myportfolio/src/comportment/Business.jsx
@@ -5,7 +5,7 @@ import { BsBuilding, BsGithub, BsGlobe, BsPerson } from "react-icons/bs";
 const Business = () => {
   return (
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
-        <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsBuilding /><span className=' italic pl-2 font-semibold'>About Me</span></h1>
+        <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsBuilding /><span className=' italic pl-2 font-semibold'>Business Ownerships</span></h1>
 
     </div>
   )
