0871db55b8e2ba4199ddca2ebd3cdd264b5c0853
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 10:05:56 +0530

    Update Experience.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/Experience.jsx
+++ myportfolio/src/comportment/Experience.jsx
@@ -6,12 +6,13 @@ const Experience = () => {
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
         <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsPersonGear /><span className=' italic pl-2 font-semibold'>Experience </span></h1>
         <div className="my-12 md:grid grid-cols-3 gap-4 md:mx-16">
-            <div className="">
+            <div className="my-8 md:my-0">
                 <div className="md:flex">
                     <img className='rounded h-40' src="https://avatars.githubusercontent.com/u/139304882?s=200&v=4" alt="" />
                     <div className="md:mx-8 md:my-0 my-2">
                         <p className="md:text-2xl text-xl text-[#00A0DF]">The JKCSS - CSS Framework</p>
                         <p className="">First Utility-First Dynamic CSS Framework in Sri Lanka</p>
+                        <p className="font-semibold text-[#00A0DF]">Founder and CEO</p>
                         <p className="my-4">2023-May - Present</p>
                         <p className="">Skills : CSS, SCSS, Framework Development</p>
                         <p className="flex my-4">
@@ -31,6 +32,39 @@ const Experience = () => {
                     </div>
                 </div>
             </div>
+            <hr className='md:hidden block border border-[#00A0DF] w-3/4'/>
+
+            <div className="my-8 md:my-0">
+                <div className="md:flex">
+                    <img className='rounded h-40' src="https://avatars.githubusercontent.com/u/139304882?s=200&v=4" alt="" />
+                    <div className="md:mx-8 md:my-0 my-2">
+                        <p className="md:text-2xl text-xl text-[#00A0DF]">The JKCSS - CSS Framework</p>
+                        <p className="">First Utility-First Dynamic CSS Framework in Sri Lanka</p>
+                        <p className="font-semibold text-[#00A0DF]">Founder and CEO</p>
+                        <p className="my-4">2023-May - Present</p>
+                        <p className="">Skills : CSS, SCSS, Framework Development</p>
+                        <p className="flex my-4">
+                            <a href="https://github.com/JKCSS-CSS-Framework" className='px-2 text-xl text-[#00A0DF]' target='_blank'>
+                                <BsGithub />
+                            </a>
+                            <a href="https://jkcss-css-framework.github.io/JKCSS-Framework/site/content/docs/" className='px-2 text-xl text-[#00A0DF]' target='_blank'>
+                                <BsGlobe />
+                            </a>
+                            <a href="https://www.linkedin.com/company/jkcss/posts/?feedView=all" className='px-2 text-xl text-[#00A0DF]' target='_blank'>
+                                <BsLinkedin />
+                            </a>
+                            <a href="https://www.npmjs.com/package/@jehankandy/jkcss" className='px-2 text-xl text-[#00A0DF]' target='_blank'>
+                                <BsBoxSeam />
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <hr className='md:hidden block border border-[#00A0DF] w-3/4'/>
+
+
+
+
         </div>
     </div>
   )
