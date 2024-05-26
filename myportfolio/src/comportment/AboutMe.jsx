5b126848547114011d4caec29cff575e07f66153
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 08:01:40 +0530

    Update AboutMe.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/AboutMe.jsx
+++ myportfolio/src/comportment/AboutMe.jsx
@@ -13,7 +13,9 @@ const AboutMe = () => {
                             <img src="https://avatars.githubusercontent.com/u/111488170?s=200&v=4" alt="" className='rounded md:h-80 h-40'/>
                             <p className="py-4 text-xl">First Utility-First Dynamic CSS Framework in Sri Lanka</p>
                             <p className="text-[#00A0DF] font-semibold text-3xl">JKCSS</p>
-                            <p className=""><BsGlobe /></p>
+                            <a href="" target='_blank'>
+                                <p className="text-2xl font-semibold my-2"><BsGlobe /></p>
+                            </a>
                         </center>
                     </div>
                 </div>
