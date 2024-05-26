ec875e3fd777a6cd55c1588b72b47a359af59ef0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 09:28:58 +0530

    Update Business.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/Business.jsx
+++ myportfolio/src/comportment/Business.jsx
@@ -6,7 +6,9 @@ const Business = () => {
   return (
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
         <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsBuilding /><span className=' italic pl-2 font-semibold'>Business Ownerships</span></h1>
+        <div className="my-12 md:grid grid-cols-3 gap-4 md:mx-16">
 
+        </div>
     </div>
   )
 }
