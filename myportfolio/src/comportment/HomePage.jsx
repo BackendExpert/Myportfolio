a34e8deb85a1f3bdaa66e686c15f14036f5f53a2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 08:00:03 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/HomePage.jsx
+++ myportfolio/src/comportment/HomePage.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
                 <div className="mx-16 my-64">
                     <h1 className="md:mt-40 md:text-5xl font-semibold">Jehan Weerasuriya</h1>
                     <p className="my-4 text-4xl text-[#00A0DF] font-semibold">Frontend && Backend Developer</p>
-                    <p className="text-xl text-white font-semibold my-4">Founder and CEO at First CSS Framework in Sri Lanka (JKCSS)</p>                    
+                    <p className="text-xl text-white font-semibold my-4">Founder and CEO of the First Sri Lankan CSS Framework (JKCSS)</p>                    
                     <p className="text-xl my-8">I am the Founder and CEO of the First CSS Framework in Sri Lanka and, the Founder and CEO of React-JKCSS, which is the First CSS Framework Developed in Sri Lanka that supports React js also the Founder and CEO of the JKReact-Icons Which supports React web development.</p>
                     <div className="my-4 flex">
                         <a href="md:block block">
@@ -46,7 +46,7 @@ const HomePage = () => {
                     <img src={MyImg} alt="" className='rounded-full h-40 w-auto mx-16 my-8'/>
                     <h1 className="text-3xl font-semibold text-white py-1">Jehan Weerasuriya</h1> 
                     <p className="text-md text-[#00A0DF] font-semibold">Frontend && Backend Developer</p>                   
-                    <p className="text-sm text-white my-4">I am the Founder and CEO of the First CSS Framework in Sri Lanka and, the Founder and CEO of React-JKCSS, which is the First CSS Framework Developed in Sri Lanka that supports React js also the Founder and CEO of the JKReact-Icons Which supports React web development.</p>
+                    <p className="text-sm text-white my-4">I am the Founder and CEO of the First Sri Lankan CSS Framework and, the Founder and CEO of React-JKCSS, which is the First CSS Framework Developed in Sri Lanka that supports React js also the Founder and CEO of the JKReact-Icons Which supports React web development.</p>
                     <div className="my-4 flex">
                         <a href="md:block block">
                             <button className='border-2 border-[#00A0DF] flex bg-[#00A0DF] text-black py-4 px-4 rounded-md '>
