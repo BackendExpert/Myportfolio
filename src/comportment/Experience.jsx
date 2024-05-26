20405655bea1267343f83505e49db1423a57dd96
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 14:36:24 +0530

    Update Experience.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/comportment/Experience.jsx
+++ src/comportment/Experience.jsx
@@ -3,7 +3,7 @@ import { BsBoxSeam, BsBuilding, BsGithub, BsGlobe, BsLinkedin, BsPerson, BsPerso
 
 const Experience = () => {
   return (
-    <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
+    <div className='md:px-10 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
         <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsPersonGear /><span className=' italic pl-2 font-semibold'>Experience </span></h1>
         <div className="my-12 md:grid grid-cols-3 gap-4 md:mx-16">
             <div className="my-8 md:my-0">
