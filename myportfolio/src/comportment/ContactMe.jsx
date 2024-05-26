95180ceec942339b3edde41df75629de7d57fa3c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 14:05:19 +0530

    Update ContactMe.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- myportfolio/src/comportment/ContactMe.jsx
+++ myportfolio/src/comportment/ContactMe.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { BsPersonLinesFill } from 'react-icons/bs'
+import { BsEnvelope, BsGithub, BsLinkedin, BsMailbox, BsPersonLinesFill, BsWhatsapp } from 'react-icons/bs'
 
 const ContactMe = () => {
   return (
     <div className='md:px-20 px-8 md:py-24 py-12 bg-[#081B28] text-white'>
         <h1 className="md:text-2xl text-2xl text-[#00A0DF] font-semibold flex "><BsPersonLinesFill /><span className=' italic pl-2 font-semibold'>Contact Me</span></h1>
-        <div className="my-12 md:grid grid-cols-3 gap-4 md:mx-16">
-
+        <div className="flex justify-between md:mx-16 my-4">
+            <h1 className="text-[#00A0DF]"><BsEnvelope className='h-20 w-auto'/></h1>
+            <h1 className="text-[#00A0DF]"><BsLinkedin className='h-20 w-auto'/></h1>
+            <h1 className="text-[#00A0DF]"><BsGithub className='h-20 w-auto'/></h1>
+            <h1 className="text-[#00A0DF]"><BsWhatsapp className='h-20 w-auto'/></h1>
         </div>
+
     </div>
   )
 }
